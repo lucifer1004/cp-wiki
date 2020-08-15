91da3a8a8c7711fdc91791cd66c3ec554cb35c57
Gabriel Wu <qqbbnease1004@126.com>
2020-08-15 16:38:34 +0800

    feat: add GA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,53 +1,59 @@
 module.exports = {
-  title: 'CP Wiki',
+  title: "CP Wiki",
   themeConfig: {
     nav: [
       {
-        text: '首页',
-        link: '/',
+        text: "首页",
+        link: "/",
       },
       {
-        text: '代数',
-        link: '/algebra/',
+        text: "代数",
+        link: "/algebra/",
       },
       {
-        text: '数据结构',
-        link: '/data-structure/',
+        text: "数据结构",
+        link: "/data-structure/",
       },
       {
-        text: '几何',
-        link: '/geometry/',
+        text: "几何",
+        link: "/geometry/",
       },
       {
-        text: '题解',
-        link: '/editorial/',
+        text: "题解",
+        link: "/editorial/",
       },
       {
-        text: '黑话',
-        link: '/jargon/',
+        text: "黑话",
+        link: "/jargon/",
       },
     ],
-    sidebar: 'auto',
+    sidebar: "auto",
   },
   markdown: {
     lineNumbers: true,
-    extendMarkdown: md => {
-      md.use(require('@iktakahiro/markdown-it-katex'));
-      md.use(require('markdown-it-container'), 'spoiler', {
-        validate: params => {
+    extendMarkdown: (md) => {
+      md.use(require("@iktakahiro/markdown-it-katex"));
+      md.use(require("markdown-it-container"), "spoiler", {
+        validate: (params) => {
           return params.trim().match(/^spoiler\s+(.*)$/);
         },
         render: (tokens, idx) => {
           var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
           if (tokens[idx].nesting === 1) {
             return (
-              '<details><summary>' + md.utils.escapeHtml(m[1]) + '</summary>\n'
+              "<details><summary>" + md.utils.escapeHtml(m[1]) + "</summary>\n"
             );
           } else {
-            return '</details>\n';
+            return "</details>\n";
           }
         },
       });
     },
   },
+  plugins: [
+    "@vuepress/google-analytics",
+    {
+      "ga": "G-86WYJ60C5M",
+    },
+  ],
 };
