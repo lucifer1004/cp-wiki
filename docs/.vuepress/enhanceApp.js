20a085339a427798d308fddead0f629a6a430f4f
Gabriel Wu <qqbbnease1004@126.com>
2020-08-31 07:30:47 +0800

    fix: ad

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,8 @@
 export default ({ Vue, options, router, siteData, isServer }) => {
-  if (!isServer) {
-    import('vue-google-adsense').then(Ads => {
-      Vue.use(require('vue-script2'));
-      Vue.use(Ads.default.Adsense);
-      Vue.use(Ads.default.InArticleAdsense);
-      Vue.use(Ads.default.InFeedAdsense);
-    });
-  }
+  import('vue-google-adsense').then(Ads => {
+    Vue.use(require('vue-script2'));
+    Vue.use(Ads.default.Adsense);
+    Vue.use(Ads.default.InArticleAdsense);
+    Vue.use(Ads.default.InFeedAdsense);
+  });
 };
